c7744bf5d19310adda8cdd50711f742c5ee22c4d
dustyRose12 <dustyrose125@gmail.com>
2017-08-21 19:26:45 -0500

    added cook the pasta first

--- recipe.rb
+++ recipe.rb
@@ -1,3 +1,4 @@
-1. First, prep the veggies.
-2. Sautee the veggies on med-high.
-3. Add veggies to cooked pasta.
+1. Cook pasta for 8 minutes.
+2. First, prep the veggies.
+3. Sautee the veggies on med-high.
+4. Add veggies to cooked pasta.
